a116a6ff8bdb955d110842b0cef73a7341cfc138
Joshua Benefield <benefield.joshua@gmail.com>
2021-12-07 14:27:40 -0700

    Adjust landing page to show cards

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import Card from '../Card/Card'
 import './LandingPage.css'
 
 const LandingPage = () => {
+  const cards = [
+    <Card />,
+    <Card />,
+    <Card />,
+    <Card />,
+    <Card />,
+    <Card />,
+    <Card />,
+  ]
+
   return (
     <main className='landing-page'>
+      <section className='emergency-instructions'>
+        <h2>EMERGENCY INSTRUCTIONS</h2>
+      </section>
       <section className='landing-form-container'>
         <h2>What did your pet eat?</h2>
         <form className='landing-form'>
@@ -14,6 +28,9 @@ const LandingPage = () => {
           />
           <button className='landing-form-btn'>Search</button>
         </form>
+      </section>
+      <section className='item-cards-container'>
+        { cards }
       </section>
     </main>
   )
