d18132b7eb8c5f8c911b5734bf996b4c10cdeca6
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-12-07 16:54:12 -0600

    Create suggestion forms and buttons

--- src/components/SuggestionPage/SuggestionPage.js
+++ src/components/SuggestionPage/SuggestionPage.js
@@ -4,7 +4,18 @@ import './SuggestionPage.css'
 const SuggestionPage = () => {
   return (
     <main className='suggestion-page'>
-
+      <form className='suggestion-form'>
+        <h2>Make a suggestion</h2>
+        <input className="suggestion-title"
+          type='text'
+          placeholder='Suggestion title'
+        />
+        <textarea className="suggestion-description"
+          type='text'
+          placeholder='Description of the suggestion'
+        />
+        <button>Submit Suggestion</button>
+      </form>
     </main>
   )
 }
