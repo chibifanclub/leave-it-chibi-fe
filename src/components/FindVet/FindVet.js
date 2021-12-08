196c9b912a19002c4ae6345d27a13ac4f5eed9f4
Joshua Benefield <benefield.joshua@gmail.com>
2021-12-08 00:43:10 -0700

    Create inputs that can auto fill, and generate results with location package

--- src/components/FindVet/FindVet.js
+++ src/components/FindVet/FindVet.js
@@ -1,10 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FindVet.css'
+import { Country, State, City } from 'country-state-city'
+import { ReactSearchAutocomplete } from 'react-search-autocomplete'
 
 const FindVet = () => {
+
+
+  const getStates = () => {
+    return (
+      State.getStatesOfCountry('US').map((state, index) => {
+        return({
+          id: index,
+          name: state.name
+        })
+      })
+    )
+  }
+  const getCities = city => {
+    return (
+      City.getCitiesOfState('US', city).map((city, index) => {
+        return ({
+          id: index,
+          name: city.name
+        })
+      })
+    )
+  }
+
+  const [selectedState, updateSelectedState] = useState('')
+  const [cities, updateCities] = useState('')
+
+  const handleOnSelect = item => {
+    updateSelectedState(State.getStatesOfCountry('US')[item.id])
+  }
+
   return (
     <main className='find-vet-page'>
-
+      <form className='find-vet-form'>
+        <h2>Find a Vet</h2>
+        <label>State</label>
+        <div className='state-input'>
+          <ReactSearchAutocomplete
+            items={getStates()}
+            maxResults='2'
+            onSelect={handleOnSelect}
+          />
+        </div>
+        <>
+          {
+            selectedState &&
+            <div className='state-input'>
+              <label>City</label>
+              <ReactSearchAutocomplete
+                items={getCities(selectedState.isoCode)}
+                maxResults='2'
+              />
+            </div>
+          }
+        </>
+      </form>
     </main>
   )
 }
